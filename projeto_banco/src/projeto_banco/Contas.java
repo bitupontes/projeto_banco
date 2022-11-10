08ebd902f4f606e9596ad79d72460e253f9b4475
etec <etec@lab02-10>
2022-11-09 22:31:02 -0300

    Atualização

--- projeto_banco/src/projeto_banco/Contas.java
+++ projeto_banco/src/projeto_banco/Contas.java
@@ -5,6 +5,8 @@ public class Contas {
 	int numero;
 	double saldo;
 
+	private static int valorRandom = 1; // Mantem alterações
+	
 	void setSaque(double valor) {
 		
 		if (valor <= saldo) {
@@ -14,6 +16,12 @@ public class Contas {
 	
 	double getSaldo() {
 		return this.saldo;
+	}
+	
+	public void setRandom() {
+		this.numero = valorRandom;
+		valorRandom++;
+		
 	}
 	
 }
