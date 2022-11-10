ada7726fe61db747e8187c8225e6ccf6e56a9eb7
etec <etec@lab02-10>
2022-11-09 22:45:53 -0300

    Func

---
+++ projeto_banco/src/projeto_banco/Funcionarios.java
@@ -0,0 +1,12 @@
+package projeto_banco;
+
+public class Funcionarios extends Cliente {
+
+	public Funcionarios(String nome, String cpf) {
+		super(nome, cpf);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+			
+}
